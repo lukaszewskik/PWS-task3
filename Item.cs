37d82ca5b15a86dc8aa736308f10c1c885485e80
lukaszewskik <51677513+lukaszewskik@users.noreply.github.com>
2021-02-22 21:53:50 +0200

    Item

--- Item.cs
+++ Item.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+public class Item
 {
     public string name;
     public string description;
     public int value;
+    public Sprite sprite; 
 
+    
 
 }
