f45ebd7efeab282bb46980798d3c39ecb5d726e0
lukaszewskik <51677513+lukaszewskik@users.noreply.github.com>
2021-02-22 20:20:44 +0200

    First version of inventory

---
+++ InventoryComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryComponent : MonoBehaviour
+{
+    public int money;
+    public List<Item> items;
+
+    public void ShowInventory()
+    {
+        Debug.Log("Money: " + money);
+        foreach (Item item in items)
+        {
+            Debug.Log(item.name + ", " + item.description);
+        }
+    }
+
+    void SelectItem(Item item)
+    {
+        
+    }
+
+
+
+
+}
---
+++ ShopComponent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopComponent : MonoBehaviour
+{
+    InventoryComponent inventory;
+
+    private void Start()
+    {
+        inventory = GetComponent<InventoryComponent>();
+    }
+
+    void BuyItem(Item item, InventoryComponent targetInventory)
+    {
+        if (inventory.money >= item.value)
+        {
+            inventory.money -= item.value;
+            targetInventory.money += item.value;
+            inventory.items.Add(item);
+            targetInventory.items.Remove(item);
+        }
+    }
+
+    void SellItem(Item item, InventoryComponent targetInventory)
+    {
+        if (targetInventory.money >= item.value)
+        {
+            inventory.money += item.value;
+            targetInventory.money -= item.value;
+            inventory.items.Remove(item);
+            targetInventory.items.Add(item);
+        }
+    }
+}
